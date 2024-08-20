b2f275dd34478c1d0c580d07bae573add67a6a70
Boris Perez <borisperezg@ufps.edu.co>
2024-08-20 15:13:52 -0500

    agregando cosas

---
+++ src/p2/matricula/modelo/Universidad.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p2.matricula.modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Boris Perez
+ */
+public class Universidad {
+    
+    private ArrayList<ProgramaAcademico> programas;
+    private ArrayList<Estudiante> estudiantes;
+
+    public Universidad() {
+        programas = new ArrayList<ProgramaAcademico>();
+        estudiantes = new ArrayList<Estudiante>();
+    }
+    
+    
+    
+}
